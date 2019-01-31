285dafd8fb7c58b0442ebd0977d143e12ec02a02
Łukasz Oleś <lukaszoles@gmail.com>
2019-02-01 00:18:26 +0100

    Fix scripts for starting local docker registry

--- hack/setup-docker-registry.sh
+++ hack/setup-docker-registry.sh
@@ -1,3 +1,3 @@
 kubectl apply -f hack/kube-docker-registry.yaml
-kubectl wait pod/$(kubectl get po -n kube-system | grep kube-registry-v0 | awk '{print $1;}') -n kube-system --for condition=Ready --timeout=180s
-kubectl port-forward --namespace kube-system $(kubectl get po -n kube-system | grep kube-registry-v0 | awk '{print $1;}') 30500:5000
+kubectl wait pod/$(kubectl get po -n kube-system | grep docker-registry | awk '{print $1;}') -n kube-system --for condition=Ready --timeout=180s
+kubectl port-forward --namespace kube-system $(kubectl get po -n kube-system | grep docker-registry | awk '{print $1;}') 30500:5000
