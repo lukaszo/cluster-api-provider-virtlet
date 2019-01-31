f01bbf2d15c96df27b56ffa1547284d340892036
Łukasz Oleś <lukaszoles@gmail.com>
2019-01-31 10:52:17 +0100

    Update path to yaml file

--- hack/setup-docker-registry.sh
+++ hack/setup-docker-registry.sh
@@ -1,3 +1,3 @@
-kubectl apply -f kube-docker-registry.yaml
+kubectl apply -f hack/kube-docker-registry.yaml
 kubectl wait pod/$(kubectl get po -n kube-system | grep kube-registry-v0 | awk '{print $1;}') -n kube-system --for condition=Ready --timeout=180s
 kubectl port-forward --namespace kube-system $(kubectl get po -n kube-system | grep kube-registry-v0 | awk '{print $1;}') 30500:5000
